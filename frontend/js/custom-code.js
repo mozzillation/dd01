be4ac3f66d36a489d414b63b3181ed76bb29102a
mozzillation <giulianomozzillo@live.it>
2018-10-09 17:18:21 +0200

    header

--- frontend/js/custom-code.js
+++ frontend/js/custom-code.js
@@ -1,43 +1,21 @@
-
 $(window).load(function() {
-
-  $('body').addClass('is-visible');
-
-  $('.lazy').Lazy({
-  scrollDirection: 'vertical',
-  effect: "fadeIn",
-  effectTime: 500,
-  threshold: 100,
-    onError: function(element) {
-        console.log('error loading ' + element.data('src'));
-    }
-  });
-
-  $('.play').yu2fvl();
-
-	$('.header__mobile, .mobile__menu_close').click(function() {
-		$(this).toggleClass('is-open');
-		$('.mobile__menu').toggleClass('is-open');
-	});
-
-	$('.template__home_carousel').slick({
-	  infinite: true,
-	  slidesToShow: 2,
-	  slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		arrows: false,
-		dots: true,
-		responsive: [
-		 {
-			 breakpoint: 500,
-			 settings: {
-				 slidesToShow: 1,
-				 slidesToScroll: 1,
-				 infinite: true,
-				 dots: true
-			 }
-		 }]
-	});
-
+  animate();
 })
+
+function animate() {
+  var i = 0,
+    arrayAnimation = $('[data-animate]').toArray();
+
+  console.log(arrayAnimation);
+
+  arrayAnimation.forEach(function(t) {
+    var delay = i * 100;
+
+    setTimeout(function() {
+      t.classList.add('is-animated');
+    }, delay);
+
+    i++;
+  })
+
+};
