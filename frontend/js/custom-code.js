d97c712c62945109504751028c2b92fd24ac814a
mozzillation <giulianomozzillo@live.it>
2018-10-09 23:46:11 +0200

    a lot of thingz

--- frontend/js/custom-code.js
+++ frontend/js/custom-code.js
@@ -1,6 +1,49 @@
+var header = $('.header');
+
+
+
+
+
 $(window).load(function() {
-  animate();
+
+  $("html, body").animate({
+    scrollTop: 0
+  }, 10);
+
+  setTimeout(function() {
+    animate();
+  }, 1000)
+
+  var wHeight = $(window).height();
+
+  $(window).scroll(function() {
+
+    var scroll = $(window).scrollTop();
+
+    if (scroll > wHeight / 2) {
+      $('video')[0].pause();
+    } else {
+      $('video')[0].play();
+    }
+
+    var opacityValue = map(scroll, 0, wHeight / 1.5, 0, 1);
+    var positionValue = map(scroll, 0, wHeight / 1.5, 0, -100);
+
+    $('.overlay').css({
+      opacity: opacityValue
+    })
+
+    header.css({
+      transform: "translateY(" + positionValue + "px)"
+    })
+
+
+  })
+
 })
+
+
+
 
 function animate() {
 
@@ -25,4 +68,26 @@ function animate() {
     i++;
   })
 
+  inView.offset({
+    top: 0,
+    right: 0,
+    bottom: 50,
+    left: 0
+  });
+  inView.threshold(1);
+  inView('[data-scroll]')
+    .on('enter', el => {
+      console.log(el);
+      el.style.opacity = 1;
+      el.classList.add('is-animated')
+    })
+    .on('exit', el => {
+      // el.style.opacity = 0.1;
+    });
+
 };
+
+
+function map(num, in_min, in_max, out_min, out_max) {
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
